5d93fa8167661797eb3fafc3beaccb185e6c2cc5
ghali <ez.zahed.abdelaziz@gmail.com>
2022-12-16 23:25:34 +0100

    added text.c

---
+++ text.c
@@ -0,0 +1,12 @@
+// sass : Syntactically Awesome Stylesheet.
+// is a powerful CSS preprocessor (generate css style) scripting language that helps you to
+// work on your style sheet much faster than ever.
+// Sass allows you to use features such as variables, function
+// used to make dark modes
+// static to dynamic language
+// used to creat css library
+// css prefix (-webkit-,-moz-) is are a way for browser makers to add support for new CSS features before those 
+    //features are fully supported in all browsers
+// when work in a bage project if creat your own css inside the current project you well have allot of trouble the classes you added  can be lready exeste ;
+// mai.css is the production version 
+// main.scss is the development version
